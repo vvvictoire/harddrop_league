af5dc37bca2c79c86732640fb96a3ca0bb7edf93
Victoire <vvictoire@pm.me>
2019-06-04 01:39:29 +0200

    autodetect if in prod or not

--- league_site/settings.py
+++ league_site/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = len(sys.argv) > 1 and sys.argv[1] == 'runserver'
+#DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if DEBUG:
