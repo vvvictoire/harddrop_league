f691b78d59cd175b42fb48fb299b92b178b8bc4b
Victoire <vvictoire@pm.me>
2019-06-07 16:16:41 +0200

    Linting discord_bot

--- read_only_site/management/commands/discord_bot.py
+++ read_only_site/management/commands/discord_bot.py
@@ -1,42 +1,50 @@
-from django.core.management.base import BaseCommand, CommandError
-from read_only_site.models import Player, Match
-import discord
+"""Manages the discord_bot command"""
+
 import os
+
+from django.core.management.base import BaseCommand
+
 from discord.ext import commands
+from read_only_site.models import Match, Player
 
-bot = commands.Bot(command_prefix='!')
+BOT = commands.Bot(command_prefix='!')
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '_discord_token'), 'r') as file:
-    token = file.read().replace('\n', '')
+    TOKEN = file.read().replace('\n', '')
 
-@bot.command()
+@BOT.command()
 async def register(context, jstris_handle: str):
+    """Registers a new player"""
     player = str(context.message.author)
     nickname = context.message.author.display_name
     mention = context.message.author.mention
     # check if player already exists
-    new_player, created = Player.objects.get_or_create(discord_handle=player,
-                                                       discord_nickname=nickname,
-                                                       jstris_handle=jstris_handle)
+    _, created = Player.objects.get_or_create(discord_handle=player,
+                                              discord_nickname=nickname,
+                                              jstris_handle=jstris_handle)
     if created:
         await context.send(mention + ' is now registered!')
     else:
         await context.send(mention + ', you already were registered you cheeky m8')
 
-@bot.command()
+@BOT.command()
 async def winner(context):
-    loser = str(context.message.author)
+    """Register a match, mentionning the winner by the loser"""
+    loser_handle = str(context.message.author)
     loser_mention = context.message.author.mention
-    if len(context.message.mentions < 1):
+    if not context.message.mentions:
         await context.send('You must mention the winner!')
-    winner = str(context.message.mentions[0])
+        return
+    winner_handle = str(context.message.mentions[0])
     winner_mention = context.message.mentions[0].mention
     try:
-        player_loser = Player.objects.get(discord_handle=loser)
+        player_loser = Player.objects.get(discord_handle=loser_handle)
     except Player.DoesNotExist:
-        await context.send(loser_mention + ', you are not registered in the league! Type !register to register in the league.')
+        await context.send(loser_mention + ', you are not registered in the'
+                                           'league! Type !register to register'
+                                           'in the league.')
         return
     try:
-        player_winner = Player.objects.get(discord_handle=winner)
+        player_winner = Player.objects.get(discord_handle=winner_handle)
     except Player.DoesNotExist:
         await context.send(winner_mention + ' is not (yet) registered in the league!')
         return
@@ -45,7 +53,8 @@ async def winner(context):
     await context.send('Match saved!')
 
 class Command(BaseCommand):
+    """Command class to manage the discord_bot command"""
     help = 'Launches the discord bot'
 
     def handle(self, *args, **options):
-        bot.run(token)
+        BOT.run(TOKEN)
